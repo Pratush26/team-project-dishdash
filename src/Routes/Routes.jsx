5ac59461c6a68c14f23f67a533dc1255aa08d6be
Pratush <pratushbarua26@gmail.com>
2026-01-02 15:00:22 +0600

    added restaurant form

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,8 @@ import HomeLayout from "../Layouts/Home";
 import HomePage from "../Pages/Home";
 import RegisterPage from "../Pages/Forms/Register";
 import LoginPage from "../Pages/Forms/Login";
+import OpenRestaurantForm from "../Pages/Forms/OpenRestaurent";
+import ManageRestaurant from "../Pages/tables/ManageRestaurant";
 
 export const router = createBrowserRouter([
     {
@@ -20,6 +22,14 @@ export const router = createBrowserRouter([
             {
                 path: "login",
                 Component: LoginPage
+            },
+            {
+                path: "open-restaurant",
+                element: <OpenRestaurantForm />
+            },
+            {
+                path: "manage-restaurant",
+                element: <ManageRestaurant />
             },
         ]
     }
